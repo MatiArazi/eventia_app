b1f53bae28a2c4434f06c1c7d4a40d238f399c42
Matias Arazi <matiasarazi@gmail.com>
2024-10-27 14:09:35 -0300

    Migrate to Google Fonts, remove local font files, and update theme

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
 
 ThemeData buildThemeData() {
@@ -11,69 +12,70 @@ ThemeData buildThemeData() {
       onPrimary: Colors.white,
     ),
     scaffoldBackgroundColor: AppConstants.background,
-    fontFamily: 'Poppins',
-    textTheme: const TextTheme(
-      // Estilos de texto para encabezados principales (antes headline1)
-      displayLarge: TextStyle(
-        fontFamily: 'Poppins',
-        fontSize: 32.0,
-        fontWeight: FontWeight.bold,
-        color: AppConstants.textPrimary,
+    textTheme: TextTheme(
+      // Usamos Google Fonts para aplicar Poppins o Roboto según sea necesario
+      displayLarge: GoogleFonts.poppins(
+        textStyle: const TextStyle(
+          fontSize: 32.0,
+          fontWeight: FontWeight.bold,
+          color: AppConstants.textPrimary,
+        ),
       ),
-      displayMedium: TextStyle(
-        fontFamily: 'Poppins',
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
-        color: AppConstants.textPrimary,
+      displayMedium: GoogleFonts.poppins(
+        textStyle: const TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+          color: AppConstants.textPrimary,
+        ),
       ),
-      // Estilos de texto para cuerpo (antes bodyText1 y bodyText2)
-      bodyLarge: TextStyle(
-        fontFamily: 'Roboto',
-        fontSize: 16.0,
-        color: AppConstants.textSecondary,
+      bodyLarge: GoogleFonts.roboto(
+        textStyle: const TextStyle(
+          fontSize: 16.0,
+          color: AppConstants.textSecondary,
+        ),
       ),
-      bodyMedium: TextStyle(
-        fontFamily: 'Roboto',
-        fontSize: 14.0,
-        color: AppConstants.textSecondary,
+      bodyMedium: GoogleFonts.roboto(
+        textStyle: const TextStyle(
+          fontSize: 14.0,
+          color: AppConstants.textSecondary,
+        ),
       ),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppConstants.primary, // Color del botón elevado
         foregroundColor: Colors.white, // Color del texto del botón
-        padding: EdgeInsets.all(AppConstants.padding), // Padding del botón
-        textStyle: const TextStyle(
-          fontFamily: 'Poppins',
-          fontSize: 18.0,
-          fontWeight: FontWeight.w600,
+        padding: const EdgeInsets.all(AppConstants.padding),
+        textStyle: GoogleFonts.poppins(
+          textStyle: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
         ),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(
-              AppConstants.borderRadius), // Borde redondeado
+          borderRadius: BorderRadius.circular(AppConstants.borderRadius),
         ),
       ),
     ),
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(
-            AppConstants.borderRadius), // Borde redondeado
+        borderRadius: BorderRadius.circular(AppConstants.borderRadius),
         borderSide: const BorderSide(
           color: AppConstants.primary,
         ),
       ),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(
-            AppConstants.borderRadius), // Borde redondeado
+        borderRadius: BorderRadius.circular(AppConstants.borderRadius),
         borderSide: const BorderSide(
           color: AppConstants.secondary,
           width: 2.0,
         ),
       ),
-      labelStyle: const TextStyle(
-        fontFamily: 'Roboto',
-        fontSize: 16.0,
-        color: Colors.black54,
+      labelStyle: GoogleFonts.roboto(
+        textStyle: const TextStyle(
+          fontSize: 16.0,
+          color: Colors.black54,
+        ),
       ),
     ),
   );
